f6e5237c252436125bf3574d4bdf367243a646db
dotdiego <dev@dm-it.fr>
2021-12-09 11:13:29 +0100

    Adding nightwind to automatically handle tailwind dark mode

--- src/DotDiego/postcss.config.js
+++ src/DotDiego/postcss.config.js
@@ -1,6 +1,6 @@
 ﻿module.exports = {
     plugins: {
-        tailwindcss: {},
+        tailwindcss: {config: './tailwind.config.js'},
         autoprefixer: {}
     }
 }
--- src/DotDiego/tailwind.config.js
+++ src/DotDiego/tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: (process.env.ASPNETCORE_ENVIRONMENT === 'production'),
         content: [
             './**/*.html',
             './**/*.razor'
         ],
     },
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("nightwind")],
 }
